d870da9ba2a5e3bc681cb78eb2f57646544d3018
loveactualry <loveactualry@gmail.com>
2020-07-14 19:03:52 +0900

    Create visualization.js

---
+++ visualization.js
@@ -0,0 +1,8 @@
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.sankey import Sankey
+
+# basic sankey chart
+Sankey(flows=[0.25, 0.15, 0.60, -0.20, -0.15, -0.05, -0.50, -0.10], labels=['', '', '', 'First', 'Second', 'Third', 'Fourth', 'Fifth'], orientations=[-1, 1, 0, 1, 1, 1, 0,-1]).finish()
+plt.title("Sankey diagram with default settings")
