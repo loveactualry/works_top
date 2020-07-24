baff2aa58f2df6a2eade4c94e6bd8172a9a6d1e6
loveactualry <loveactualry@gmail.com>
2020-07-24 15:55:12 +0900

    Update R.r

--- R.r
+++ R.r
@@ -1,3 +1,30 @@
 library(tidyverse)
 
 data %>% group_by(val1) %>% count() %>% pivot_wider(va1_1, n)..
+
+
+mtcars$name <- row.names(mtcars)
+
+
+
+mtcars %>% 
+
+  select(name, mpg, cyl, hp) %>% 
+
+  mutate(
+
+    type = case_when(
+
+      cyl >= 8 | hp >= 180 ~ "big",          # or
+
+      cyl >= 4 & hp >= 120 ~ "medium", # and
+
+      TRUE ~ "small"
+
+    )
+
+  )
+
+
+
+## 출처: https://rfriend.tistory.com/522 [R, Python 분석과 프로그래밍의 친구 (by R Friend)]
