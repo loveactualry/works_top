6e0bcf591bf1686122f29c98595992e46a2630d1
loveactualry <loveactualry@gmail.com>
2020-10-30 13:21:36 +0900

    x-axis modify

---
+++ matplotlib.py
@@ -0,0 +1,18 @@
+
+#https://big-blog.tistory.com/773
+
+#matplot lib x 축 custom 수정
+
+#수정하려는 축이 object형이 아닌지 확인
+#object면 int 혹은 floast로 변경
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+
+x = [0,5,9,10,15]
+y = [0,1,2,3,4]
+fig, ax = plt.subplots()
+ax.plot(x,y)
+loc = plticker.MultipleLocator(base=1.0) # this locator puts ticks at regular intervals
+ax.xaxis.set_major_locator(loc)
+plt.show()
