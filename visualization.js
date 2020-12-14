89c4f0cc0714e5704f365f8efeb6cfdcc2a4ff34
loveactualry <loveactualry@gmail.com>
2020-12-14 11:07:46 +0900

    Update visualization.js

--- visualization.js
+++ visualization.js
@@ -6,3 +6,7 @@ from matplotlib.sankey import Sankey
 # basic sankey chart
 Sankey(flows=[0.25, 0.15, 0.60, -0.20, -0.15, -0.05, -0.50, -0.10], labels=['', '', '', 'First', 'Second', 'Third', 'Fourth', 'Fifth'], orientations=[-1, 1, 0, 1, 1, 1, 0,-1]).finish()
 plt.title("Sankey diagram with default settings")
+
+
+# 다중막대그래프
+https://developer-ankiwoong.tistory.com/204
